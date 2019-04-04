5fe10dda2b7e933830fe14dcafa88a0125bea636
colodoo <lodog1@qq.com>
2019-04-04 17:11:41 +0800

    项目初始化windows脚本

--- script/lanb.bat
+++ script/lanb.bat
@@ -1,29 +1,33 @@
-@echo off
+@echo ofF
 chcp 65001
 
-if "%1%"=="" (
+set projectName=%1
+
+if "%projectName%"=="" (
 
   echo Project name is `NULL`
 
-) else (  
-  
+) else (
+
   if exist pom_tmp.xml (rm pom_tmp.xml)
-  if exist rm .project_tmp (rm rm .project_tmp)
+  if exist .project_tmp (rm rm .project_tmp)
 
   setlocal EnableDelayedExpansion
   for /f "delims=" %%i in (.project) do (
     set var=%%i
-    set "var=!var:lanb-web-be=%1%!"
+    set "var=!var:lanb-web-be=%projectName%!"
     echo !var!>>.project_tmp
   )
 
   for /f "delims=" %%i in (pom.xml) do (
     set var=%%i
-    set var=!var:LanbWeb=%1%!
+    set var=!var:LanbWeb=%projectName%!
     echo !var!>>pom_tmp.xml
   )
 
   echo yes|move pom_tmp.xml ..\pom.xml
   echo yes|move .project_tmp ..\.project
-  
+
 )
+
+echo Please input `chcp 936` by yourself
